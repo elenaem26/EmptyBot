d039b5f9887924ed800fe3b66fe043ac664f820e
elenaem <elenaem26@gmail.com>
2025-08-13 18:26:22 +0400

    jooq and flyway configuration (do not connect to db on the build stage)

---
+++ src/main/java/com/example/emptybot/configuration/JooqConfig.java
@@ -0,0 +1,17 @@
+package com.example.emptybot.configuration;
+
+import org.jooq.conf.RenderNameCase;
+import org.jooq.conf.RenderQuotedNames;
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JooqConfig {
+    @Bean
+    DefaultConfigurationCustomizer jooqCustomizer() {
+        return c -> c.settings()
+                .withRenderQuotedNames(RenderQuotedNames.NEVER)
+                .withRenderNameCase(RenderNameCase.LOWER);
+    }
+}
