896ee539d3344506e7ca63ee5080b3b09a81bf7e
elenaem <elenaem26@gmail.com>
2025-08-27 18:18:19 +0400

    debug

--- src/main/java/com/example/emptybot/configuration/VisionConfig.java
+++ src/main/java/com/example/emptybot/configuration/VisionConfig.java
@@ -3,6 +3,8 @@ package com.example.emptybot.configuration;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.vision.v1.ImageAnnotatorClient;
 import com.google.cloud.vision.v1.ImageAnnotatorSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +15,13 @@ import java.util.Base64;
 @Configuration
 public class VisionConfig {
 
+    private final Logger logger = LoggerFactory.getLogger(VisionConfig.class);
+
     @Bean
     public ImageAnnotatorClient imageAnnotatorClient() throws Exception {
-        String b64 = System.getenv("GСV_KEY");
+        String b64 = System.getenv("GCV_KEY");
+        logger.info("GCV_KEY: {}", b64 == null ? "null" : "not null");
+
         ImageAnnotatorSettings.Builder builder = ImageAnnotatorSettings.newBuilder();
 
         if (b64 != null && !b64.isBlank()) {
